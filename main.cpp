6cee96a49c6fc20f56fea91a8b98893fd2314031
SarunasMif <sarunas.gikys@mif.stud.vu.lt>
2024-02-12 17:37:02 +0200

    Students name and surname input system and input check
    Implemented the input system that allows user to enter the students name and surname.
    Implemented a system that insures that the name and surname input system contains only letters.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,67 @@
 
 using namespace std;
 
-int main(){
+const int Number_Of_Students = 3;
+
+struct Student_Data{
+    string name;
+    string surname;
+    int HW_grades[3];
+    int exam_grade;
+};
+
+Student_Data Sdata[Number_Of_Students]; 
+
+bool isString(const string& str_placeholder){
+    for (char c : str_placeholder) {
+        if (!isalpha(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void collect_Data() 
+{
+    string str_placeholder;
     
+    for (int i = 0; i < Number_Of_Students; i++){
+        do {
+            cout << "Enter the students name: ";
+            cin >> str_placeholder;
+
+            if (!isString(str_placeholder)){
+                cout << "Invalid!. Students name can not contain any numbers." << endl;
+            } 
+        }while (!isString(str_placeholder));
+
+        Sdata[i].name = str_placeholder;
+
+        do {
+            cout << "Enter the students surname: ";
+            cin >> str_placeholder;
+
+            if (!isString(str_placeholder)){
+                cout << "Invalid!. Students surname can not contain any numbers." << endl;
+            }
+        }while (!isString(str_placeholder));
+
+        Sdata[i].surname = str_placeholder;
+    }
+
+}
+
+void print_Data()
+{
+    for (int i = 0; i < Number_Of_Students; i++){
+        cout << "[" << i << "] " << Sdata[i].name << " " << Sdata[i].surname << endl;
+    }
+}
+
+int main()
+{
+    collect_Data();
+    print_Data();
+
+    return 0;
 }
